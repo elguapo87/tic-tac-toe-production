aeb941cadd828e9161cd033225ecad73c6c763ef
elguapo87 <pglogovac@gmail.com>
2025-08-25 21:24:53 +0200

    Created handleBoxClick function that controlls click for X or O & created checkWinner function

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -5,11 +5,13 @@ type Props = {
   onClickBox: (index: number) => void;
 };
 
+
+
 const Board = ({ board, onClickBox }: Props) => {
   return (
     <div className="grid grid-cols-[repeat(3,6rem)] justify-center">
       {board.map((item, index) => (
-        <Box key={index} value={item} onClickBox={() => onClickBox(index)} />
+        <Box key={index} value={item} onClickBox={() => item === null && onClickBox(index)} />
       ))}
     </div>
   )
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,10 +4,48 @@ import Board from "../components/Board"
 const Home = () => {
 
     const [board, setBoard] = useState(Array(9).fill(null));
+    const [xPlaying, setXPlaying] = useState(true);
+    const [gameOver, setGameOver] = useState(false);
+
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    const handleBoxClick = (boxIndex: number) => {
+        const updatedBoard = board.map((item, index) => {
+            if (boxIndex === index) {
+                return xPlaying ? "X" : "O";
+            } else {
+                return item;
+            }
+        });
+
+        setBoard(updatedBoard);
+        setXPlaying(!xPlaying);
+        checkWinner(updatedBoard);
+    };
+
+    const checkWinner = (board: (string | null)[]) => {
+        for (const condition of winningConditions) {
+            const [a, b, c] = condition;
+            if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+                setGameOver(true);
+                return board[a]
+            }
+        }
+    };
+
 
     return (
         <div className="relative flex flex-col">
-            <Board board={board} onClickBox={() => null} />
+            <Board board={board} onClickBox={gameOver ? () => {} : handleBoxClick} />
         </div>
     )
 }
