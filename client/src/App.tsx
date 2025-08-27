c8eb76a1ce6d53bc6266a1602195abce5441572b
elguapo87 <pglogovac@gmail.com>
2025-08-28 00:40:06 +0200

    Added checkAuth API & login/register API in AppContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import OnlineLobby from "./pages/OnlineLobby"
+import { Toaster } from "react-hot-toast"
 
 function App() {
   return (
     <div className="bg-[#16026e] min-h-screen flex flex-col text-stone-50">
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/lobby" element={<OnlineLobby />} />
--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -1,4 +1,13 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+type UserData = {
+    _id: string;
+    name: string;
+    email: string;
+    userImg: string;
+};
 
 interface AppContextType {
 
@@ -6,7 +15,61 @@ interface AppContextType {
 
 export const AppContext = createContext<AppContextType | undefined>(undefined);
 
-const AppContextProvider = ({ children } : { children: React.ReactNode }) => {
+const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+    const [token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : null);
+    const [authUser, setAuthUser] = useState<UserData | null>(null);
+
+    // Function to check if user is authenticated and if so, set the user data and connect the socketto check user auth
+    const checkAuth = async () => {
+        try {
+            const { data } = await axios.get("/api/auth/check", {
+                headers: { token }
+            });
+
+            if (data.success) {
+                setAuthUser(data.user);
+
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            console.error("checkAuth failed", error);
+        }
+    };
+
+    // Login function to handle user authentication and socket connection
+    const login = async (state: "Login" | "Sign Up", credentials: Partial<UserData>) => {
+        try {
+            const endpoint = state === "Sign Up" ? "register" : "login";
+
+            const { data } = await axios.post(`/api/auth/${endpoint}`, credentials);
+
+            if (data.success) {
+                setAuthUser(data.userData);
+                localStorage.setItem("token", data.token);
+                setToken(data.token);
+                toast.success(data.message);
+                
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            const errMessage = error instanceof Error ? error.message : "An unknown error occurred";
+            toast.error(errMessage);
+        }
+    };
+
+    useEffect(() => {
+        if (token) {
+            checkAuth();
+        }
+    }, [token])
 
     const value = {};
 
