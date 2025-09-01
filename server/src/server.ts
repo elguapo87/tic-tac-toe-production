1396052c5d73d5c0679e050d8a074365f45a1db5
elguapo87 <pglogovac@gmail.com>
2025-09-01 02:22:55 +0200

    Adjusted cors options in server.ts for deploying

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,7 +27,7 @@ registerSocketServer(io);
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 };
 app.use(cors(corsOptions));
